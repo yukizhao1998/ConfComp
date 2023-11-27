68198eac50523b416e15d1f0f011f5489a17b158
yukizhao1998 <1600012992@pku.edu.cn>
2023-11-27 11:57:20 +0800

    clean

--- prepare_data.py
+++ prepare_data.py
@@ -7,7 +7,19 @@ import os
 from issue import Issue
 from commit import *
 from pydriller import Repository, Git
+from collections import defaultdict
 
+
+def judge_isconf(file):
+    if file["new_path"]:
+        path = file["new_path"]
+    else:
+        path = file["old_path"]
+    if "documentation" in path or "xdoc" in path or "zookeeperAdmin" in path:
+        return 0
+    if str(path).endswith(".xml") or str(path).endswith(".properties"):
+        return 1
+    return 0
 
 def save_issue(conf, project, issue):
     dir = os.path.join(conf.data_path, "issues", project)
@@ -41,7 +53,7 @@ def save_clean(conf, project, clean):
     return
 
 
-def load_issue(conf):
+def collect_issue(conf):
     dfs = []
     for year in conf.years:
         df = pd.read_csv(os.path.join(conf.data_path, year + ".csv"))
@@ -98,7 +110,16 @@ def load_issue_from_json(filepath):
     return issue_list
 
 
-def filter_conf(conf):
+def load_commit_from_json(filepath):
+    commit_list_json = json.load(open(filepath, "r"))
+    commit_list = []
+    for commit_json in commit_list_json:
+        commit = Commit()
+        commit.load_from_dict(commit_json)
+        commit_list.append(commit)
+    return commit_list
+
+def filter_conf_issue(conf):
     for project in conf.projects:
         total = 0
         conf_related = 0
@@ -113,13 +134,13 @@ def filter_conf(conf):
                 conf_flag = 0
                 for fix_commit in issue.fix_commits:
                     for file in fix_commit.files:
-                        if ".xml" in file["filename"]:
+                        if judge_isconf(file):
                             conf_flag = 1
                             print("fix:", fix_commit.rows["hash"], "path:", file["new_path"])
                 if conf_flag == 1:
                     for buggy_commit in issue.buggy_commits:
                         for file in buggy_commit.files:
-                            if ".xml" in file["filename"]:
+                            if judge_isconf(file):
                                 print("buggy: ", buggy_commit.rows["hash"], "path:", file["new_path"])
                 # desc = str(issue.summary) + str(issue.description)
                 # if "config" in desc or "setting" in desc or "param" in desc:
@@ -132,7 +153,7 @@ def filter_conf(conf):
         print(total, conf_related)
 
 
-def load_clean(conf):
+def collect_clean(conf):
     clean_df = pd.read_csv(os.path.join(conf.data_path, "clean.csv"))
     for project in conf.projects:
         repo = conf.proj_repo[project][0]
@@ -152,9 +173,96 @@ def load_clean(conf):
             json.dump(visit_clean, open(os.path.join(conf.data_path, "clean", "visit_cleans.json"), "w"))
 
 
+def filter_conf_clean(conf, filter_project):
+    conf_related = []
+    type_dict = defaultdict(int)
+    for project in conf.projects:
+        total = 0
+        if project != filter_project:
+            continue
+        dir = os.path.join(conf.data_path, "clean", project)
+        filenames = os.listdir(dir)
+        for filename in filenames:
+            print(filename)
+            for clean_commit in load_commit_from_json(os.path.join(dir, filename)):
+                total += 1
+                flag = 0
+                for file in clean_commit.files:
+                    file_type = file["filename"].split(".")[-1]
+                    type_dict[file_type] += 1
+                    if judge_isconf(file):
+                        flag = 1
+                    # if (file["old_path"] and ".properties" in file["old_path"]) or (
+                    #         file["new_path"] and ".properties" in file["new_path"]):
+                    #     print(clean_commit.id)
+                    #     if file["old_path"]:
+                    #         print(file["old_path"])
+                    #     else:
+                    #         print(file["new_path"])
+                if flag:
+                    conf_related.append(clean_commit)
+        print("total changes:", total)
+        print("conf related changes:", len(conf_related))
+        print("type cnt:", str(sorted(type_dict.items(), key=lambda x: x[1])))
+    return conf_related
+
+
+def analyze_clean(conf, conf_clean):
+    conf_name_cnt = defaultdict(int)
+    conf_total = 0
+    for commit in conf_clean:
+        if 500 > commit.rows["num_lines_added"] + commit.rows["num_lines_deleted"] >= 200:
+            print(commit.id)
+        for file in commit.files:
+            if judge_isconf(file):
+                conf_name_cnt[file["filename"]] += 1
+                conf_total += 1
+    print("filename", sorted(conf_name_cnt.items(), key=lambda x: x[1], reverse=True))
+    print("diverse conf files:", str(len(conf_name_cnt.keys())))
+    print("total conf change:", conf_total)
+
+
+def calculate_chunk(diff_parsed):
+    chunk = 0
+    keys = ["added", "deleted"]
+    for key in keys:
+        l = diff_parsed[key]
+        init = -1
+        for i in l:
+            if i[0] != init + 1:
+                chunk += 1
+            init = i[0]
+    return chunk
+
+def analyze_clean_size(conf, conf_clean):
+    files = []
+    chunks = []
+    lines = []
+    for commit in conf_clean:
+        file_cnt = 0
+        chunk_cnt = 0
+        line_cnt = 0
+        for file in commit.files:
+            if judge_isconf(file):
+                file_cnt += 1
+            line_cnt += (file["num_lines_added"] + file["num_lines_deleted"])
+            chunk_cnt += calculate_chunk(file["diff_parsed"])
+        files.append(file_cnt)
+        chunks.append(chunk_cnt)
+        lines.append(line_cnt)
+    print("files:", sum(files) / len(files), files[int(len(files) / 2)])
+    print("chunks", sum(chunks) / len(chunks), chunks[int(len(chunks) / 2)])
+    print("lines", sum(lines) / len(lines), lines[int(len(lines) / 2)])
+
 
 if __name__ == "__main__":
     conf = Conf()
-    # load_issue(conf)
-    load_clean(conf)
-    filter_conf(conf)
+    # collect_issue(conf)
+    # collect_clean(conf)
+    for project in conf.projects:
+        if project not in ["ZOOKEEPER", "HBASE", "ZEPPELIN"]:
+            continue
+        conf_clean = filter_conf_clean(conf, project)
+        # analyze_clean(conf, conf_clean)
+        analyze_clean_size(conf, conf_clean)
+    # filter_conf_issue(conf)

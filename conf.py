1dcba515e4425d6a6c6b5b9f099a80840c666688
yukizhao1998 <1600012992@pku.edu.cn>
2024-01-04 18:11:58 +0800

    only collect

--- conf.py
+++ conf.py
@@ -14,7 +14,7 @@ class Conf:
         self.clean_split_epoch = 200
         self.config_file_suffix = [".xml", ".properties", ".json", ".yaml", ".yml"]
         # self.openai_api_key = "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"
-        self.openai_api_key = ""
+        self.openai_api_key = "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"
         self.label_model = "gpt-4"
         self.dt_start = datetime.datetime(2022, 2, 1, 0, 0, 0).replace(tzinfo=pytz.timezone('UTC'))
         self.dt_end = datetime.datetime(2024, 1, 1, 0, 0, 0).replace(tzinfo=pytz.timezone('UTC'))
